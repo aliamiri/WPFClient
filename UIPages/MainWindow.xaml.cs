f1b56b83501be57a122b85b814d22e48e4276339
ali amiri <ali_amiri_8131001@yahoo.com>
2015-02-03 15:51:34 +0330

    divide main in some regions
    divide main in some regions

--- UIPages/MainWindow.xaml.cs
+++ UIPages/MainWindow.xaml.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Globalization;
 using System.IO;
-using System.IO.Compression;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -27,35 +27,11 @@ namespace WpfNotifierClient.UIPages
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
-        private Logger _logger = LogManager.GetCurrentClassLogger();
-
-        private string ReadAsync()
-        {
-            while (ConnectionFlag)
-            {
-                _logger.Trace("we start reading from tcp connection");
-                var stm = _tcpClient.GetStream();
-
-                var bb = new byte[100];
-
-                var k = stm.Read(bb, 0, 100);
-                var stbldr = new StringBuilder();
-                for (var i = 0; i < k; i++)
-                    stbldr.Append(Convert.ToChar(bb[i]));
-
-                var readAsync = stbldr.ToString();
-
-                _bufferStrings += readAsync;
-                _logger.Trace("result is : " + readAsync);
-            }
-            return null;
-        }
+        #region 
 
         public delegate void ReadTcpDelegate();
 
         private readonly List<TrxInfo> _trxInfos = new List<TrxInfo>();
-
-        private List<Commands> _commands = new List<Commands>(); 
 
         private readonly TaskbarNotifier _taskbarNotifier;
 
@@ -65,6 +41,28 @@ namespace WpfNotifierClient.UIPages
         private string _bufferStrings = "";
         public static bool ConnectionFlag = false;
         public static int AccessLevel = 0;
+
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+
+        //currently this function is useless but I'll use this function in the future
+        private void CheckFistTime()
+        {
+            var appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (appPath == null) return;
+            var configFile = Path.Combine(appPath, "App.config");
+            var isFirstTime = ConfigurationManager.AppSettings["isFirstTime"];
+            if (!isFirstTime.Equals("true")) return;
+            var configFileMap = new ExeConfigurationFileMap { ExeConfigFilename = configFile };
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap,
+                ConfigurationUserLevel.None);
+            config.AppSettings.Settings["isFirstTime"].Value = "false";
+            config.Save(ConfigurationSaveMode.Full);
+            //TODO configuration for database
+            //TODO show a window to set merchant name and this type of ...
+        }
 
         public MainWindow()
         {
@@ -101,6 +99,9 @@ namespace WpfNotifierClient.UIPages
             // CheckFistTime();
         }
 
+        #region 
+        //This region is for sending log files data to server side
+
         public void UploadFtpFile()
         {
             var fileName = Path.Combine(Environment.GetFolderPath(
@@ -109,7 +110,7 @@ namespace WpfNotifierClient.UIPages
             {
                 var pureFileName = new FileInfo(fileName).Name;
                 var uploadUrl = String.Format("ftp://{0}/{1}/{2}", "localhost", "uploadHere", pureFileName);
-                var req = (FtpWebRequest)FtpWebRequest.Create(uploadUrl);
+                var req = (FtpWebRequest) FtpWebRequest.Create(uploadUrl);
                 req.Proxy = null;
                 req.Method = WebRequestMethods.Ftp.UploadFile;
                 //req.Credentials = new NetworkCredential("a.akhondian", "Aa123456%");
@@ -124,14 +125,8 @@ namespace WpfNotifierClient.UIPages
             }
             catch (Exception ex)
             {
-                _logger.Error("UploadFtpFile:" , ex.Message);
+                _logger.Error("UploadFtpFile:", ex.Message);
             }
-        }
-
-        private void UploadLogFiles()
-        {
-            ZipLogFiles();
-            UploadFtpFile();
         }
 
         private void ZipLogFiles()
@@ -140,7 +135,7 @@ namespace WpfNotifierClient.UIPages
                 Environment.SpecialFolder.LocalApplicationData), @"asan\logs");
             var zipFileAddr = Path.Combine(Environment.GetFolderPath(
                 Environment.SpecialFolder.LocalApplicationData), @"asan\logs\AsanPardakhtLogs.zip");
-            string []args = {logFolder,zipFileAddr};
+            string[] args = {logFolder, zipFileAddr};
             var filenames = Directory.GetFiles(args[0]);
             using (var s = new ZipOutputStream(File.Create(args[1])))
             {
@@ -166,33 +161,44 @@ namespace WpfNotifierClient.UIPages
             }
         }
 
+        private void UploadLogFiles()
+        {
+            ZipLogFiles();
+            UploadFtpFile();
+        }
+
+        #endregion
+
+
+        #region 
+        //This region is for TCP connection and reading data and commands from server
+
         private InputContainer InputParser()
         {
-            var container= new InputContainer();
+            var container = new InputContainer();
             try
             {
                 var indexOf = _bufferStrings.IndexOf('|');
                 if (indexOf == -1) return null;
-                
+
                 var input = _bufferStrings.Substring(0, indexOf - 1);
-                if (input.StartsWith("c"))
+                var startsWithC = input.StartsWith("c");
+                _bufferStrings = _bufferStrings.Remove(1, indexOf + 3);
+                var values = input.Split(new[] {";"}, StringSplitOptions.None);
+                if (startsWithC)
                 {
-                    _bufferStrings = _bufferStrings.Remove(1, indexOf + 3);
-                    var values = input.Split(new[] { ";" }, StringSplitOptions.None);
                     var commands = new Commands();
                     int intIn;
                     if (int.TryParse(values[0], out intIn))
-                        commands.Priority= intIn;
+                        commands.Priority = intIn;
                     if (int.TryParse(values[1], out intIn))
-                        commands.Command = (CommandTypes)Enum.ToObject(typeof(CommandTypes), intIn);
+                        commands.Command = (CommandTypes) Enum.ToObject(typeof (CommandTypes), intIn);
                     container.Command = commands;
                     container.Type = true;
                     return container;
                 }
                 else
                 {
-                    _bufferStrings = _bufferStrings.Remove(1, indexOf + 3);
-                    var values = input.Split(new[] { ";" }, StringSplitOptions.None);
                     var trxInfo = new TrxInfo();
                     int intIn;
                     if (int.TryParse(values[0], out intIn))
@@ -212,27 +218,28 @@ namespace WpfNotifierClient.UIPages
             }
         }
 
+        //TODO check redundant data
 
-        //currently this function is useless but I'll use this function in the future
-        private void CheckFistTime()
+        private string ReadAsync()
         {
-            var appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            if (appPath == null) return;
-            var configFile = Path.Combine(appPath, "App.config");
-            var isFirstTime = ConfigurationManager.AppSettings["isFirstTime"];
-            if (!isFirstTime.Equals("true")) return;
-            var configFileMap = new ExeConfigurationFileMap {ExeConfigFilename = configFile};
-            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap,
-                ConfigurationUserLevel.None);
-            config.AppSettings.Settings["isFirstTime"].Value = "false";
-            config.Save(ConfigurationSaveMode.Full);
-            //TODO configuration for database
-            //TODO show a window to set merchant name and this type of ...
-        }
+            while (ConnectionFlag)
+            {
+                _logger.Trace("we start reading from tcp connection");
+                var stm = _tcpClient.GetStream();
 
-        private bool CheckRedundantData(TrxInfo info)
-        {
-            return false;
+                var bb = new byte[100];
+
+                var k = stm.Read(bb, 0, 100);
+                var stbldr = new StringBuilder();
+                for (var i = 0; i < k; i++)
+                    stbldr.Append(Convert.ToChar(bb[i]));
+
+                var readAsync = stbldr.ToString();
+
+                _bufferStrings += readAsync;
+                _logger.Trace("result is : " + readAsync);
+            }
+            return null;
         }
 
         public string AddToListsAsync()
@@ -313,12 +320,18 @@ namespace WpfNotifierClient.UIPages
             }
         }
 
+        #endregion
+
+
         private void Reconnect_Click(object sender, RoutedEventArgs e)
         {
             ReconnectButton.Visibility = Visibility.Hidden;
             ConnectionFlag = true;
             StartTcp();
         }
+
+        #region 
+        //Menu Items
 
         private void MenuItem_lock(object sender, RoutedEventArgs e)
         {
@@ -336,14 +349,9 @@ namespace WpfNotifierClient.UIPages
 
         private void MenuItem_IntervalReport(object sender, RoutedEventArgs e)
         {
-            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(1065);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(1065);
             Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
             new IntervalReportWindow().Show();
-        }
-
-        private void MenuItem_signOut(object sender, RoutedEventArgs e)
-        {
-            SignOut();
         }
 
         private void SignOut()
@@ -361,6 +369,11 @@ namespace WpfNotifierClient.UIPages
             StartTcp();
         }
 
+        private void MenuItem_signOut(object sender, RoutedEventArgs e)
+        {
+            SignOut();
+        }
+
         private void MenuItem_createNewUser(object sender, RoutedEventArgs e)
         {
             new CreateUser().Show();
@@ -376,9 +389,10 @@ namespace WpfNotifierClient.UIPages
             ConnectAutomaticly();
         }
 
-        public delegate void ConnectTcpDelegate();
+        #endregion
 
-        private Timer _aTimer;
+
+        public delegate void ConnectTcpDelegate();
 
         private void ConnectAutomaticly()
         {
